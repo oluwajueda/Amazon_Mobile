497a2880798098cf49c138f9156cdd8de4ee0201
oluwajueda <oluwajuedaloade@gmail.com>
2023-02-19 14:44:47 +0100

    set-up server side

---
+++ server/index.js
@@ -0,0 +1,11 @@
+console.log("Hello, World");
+
+const express = require("express");
+
+const PORT = 3000;
+
+const app = express();
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`connected at port ${PORT}`);
+});
