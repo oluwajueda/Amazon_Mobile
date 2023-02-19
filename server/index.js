70b5efbff795bd4f46ff8360fca796a9b9961109
oluwajueda <oluwajuedaloade@gmail.com>
2023-02-19 15:30:18 +0100

    set up mongodb for database

--- server/index.js
+++ server/index.js
@@ -1,11 +1,28 @@
-console.log("Hello, World");
-
 const express = require("express");
+const mongoose = require("mongoose");
 
 const PORT = 3000;
 
 const app = express();
 
-app.listen(PORT, "0.0.0.0", () => {
+const DB =
+  "mongodb+srv://olamide:3435@cluster0.gkdbfpq.mongodb.net/?retryWrites=true&w=majority";
+// Imports from other files
+const authRouter = require("./routes/auth");
+
+//middleware
+app.use(authRouter);
+
+//Connections
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connection Successful");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+app.listen(PORT, () => {
   console.log(`connected at port ${PORT}`);
 });
