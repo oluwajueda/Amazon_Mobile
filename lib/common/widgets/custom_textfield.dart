4401b134f035bae860143ff2e20bb6ae45e78360
oluwajueda <oluwajuedaloade@gmail.com>
2023-02-22 10:59:55 +0100

    working on add_product

--- lib/common/widgets/custom_textfield.dart
+++ lib/common/widgets/custom_textfield.dart
@@ -7,8 +7,12 @@ import 'package:flutter/src/widgets/framework.dart';
 class CustomTextField extends StatelessWidget {
   final TextEditingController controller;
   final String hintText;
+  final int maxLines;
   const CustomTextField(
-      {super.key, required this.controller, required this.hintText});
+      {super.key,
+      required this.controller,
+      required this.hintText,
+      this.maxLines = 1});
 
   @override
   Widget build(BuildContext context) {
@@ -26,6 +30,7 @@ class CustomTextField extends StatelessWidget {
         }
         return null;
       },
+      maxLines: maxLines,
     );
   }
 }
--- lib/features/admin/screens/add_product.dart
+++ lib/features/admin/screens/add_product.dart
@@ -1,3 +1,4 @@
+import 'package:amazon_mobile/common/widgets/custom_textfield.dart';
 import 'package:amazon_mobile/constants/global_variables.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
@@ -13,6 +14,30 @@ class AddProductScreen extends StatefulWidget {
 }
 
 class _AddProductScreenState extends State<AddProductScreen> {
+  final TextEditingController productNameController = TextEditingController();
+  final TextEditingController descriptionController = TextEditingController();
+  final TextEditingController priceController = TextEditingController();
+  final TextEditingController quantityController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    productNameController.dispose();
+    descriptionController.dispose();
+    priceController.dispose();
+    quantityController.dispose();
+  }
+
+  List<String> productCategories = [
+    "Mobiles",
+    "Essentials",
+    "Appliances",
+    "Books",
+    "Fashion",
+  ];
+
+  String category = "Mobiles";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,28 +63,67 @@ class _AddProductScreenState extends State<AddProductScreen> {
                   height: 20,
                 ),
                 DottedBorder(
+                    borderType: BorderType.RRect,
+                    radius: const Radius.circular(10),
+                    dashPattern: const [10, 4],
+                    strokeCap: StrokeCap.round,
                     child: Container(
+                      width: double.infinity,
+                      height: 150,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Icon(
+                            Icons.folder_open,
+                            size: 40,
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                            "Select Product Images",
+                            style: TextStyle(
+                                fontSize: 15, color: Colors.grey.shade400),
+                          )
+                        ],
+                      ),
+                    )),
+                const SizedBox(
+                  height: 30,
+                ),
+                CustomTextField(
+                    controller: productNameController,
+                    hintText: "Product Name"),
+                const SizedBox(
+                  height: 10,
+                ),
+                CustomTextField(
+                    maxLines: 7,
+                    controller: descriptionController,
+                    hintText: "Description"),
+                const SizedBox(
+                  height: 10,
+                ),
+                CustomTextField(controller: priceController, hintText: "Price"),
+                const SizedBox(
+                  height: 10,
+                ),
+                CustomTextField(
+                    controller: quantityController, hintText: "Quantity"),
+                const SizedBox(
+                  height: 10,
+                ),
+                SizedBox(
                   width: double.infinity,
-                  height: 150,
-                  decoration:
-                      BoxDecoration(borderRadius: BorderRadius.circular(10)),
-                  child: Column(
-                    children: [
-                      const Icon(
-                        Icons.folder_open,
-                        size: 40,
-                      ),
-                      const SizedBox(
-                        height: 15,
-                      ),
-                      Text(
-                        "Select Product Images",
-                        style: TextStyle(
-                            fontSize: 15, color: Colors.grey.shade400),
-                      )
-                    ],
+                  child: DropdownButton(
+                    value: category,
+                    items: items,
+                    onChanged: onChanged,
+                    icon: const Icon(Icons.keyboard_arrow_down),
                   ),
-                ))
+                )
               ],
             ),
           ),
