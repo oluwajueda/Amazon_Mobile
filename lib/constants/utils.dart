44295372b9eebaa29ae1c859ba0064d7a5b17f7e
oluwajueda <oluwajuedaloade@gmail.com>
2023-02-24 01:49:17 +0100

    add file picker

--- lib/constants/utils.dart
+++ lib/constants/utils.dart
@@ -1,3 +1,4 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import "dart:io";
@@ -8,10 +9,21 @@ void showSnackBar(BuildContext context, String text) {
   );
 }
 
-pickImages() async {
+Future<List<File>> pickImages() async {
   List<File> images = [];
 
-  try {} catch (e) {
+  try {
+    var files = await FilePicker.platform.pickFiles(
+      type: FileType.image,
+      allowMultiple: true,
+    );
+    if (files != null && files.files.isNotEmpty) {
+      for (int i = 0; i < files.files.length; i++) {
+        images.add(File(files.files[i].path!));
+      }
+    }
+  } catch (e) {
     debugPrint(e.toString());
   }
+  return images;
 }
