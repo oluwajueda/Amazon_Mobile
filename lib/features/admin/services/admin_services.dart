9b8f48acb985745214b6acec978a681d253da213
oluwajueda <oluwajuedaloade@gmail.com>
2023-02-24 02:38:43 +0100

    add cloudinary for product upload

---
+++ lib/features/admin/services/admin_services.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+
+import 'package:amazon_mobile/constants/utils.dart';
+import 'package:cloudinary_public/cloudinary_public.dart';
+import 'package:flutter/cupertino.dart';
+
+class AdminServices {
+  void sellProduct({
+    required BuildContext context,
+    required String name,
+    required String description,
+    required double price,
+    required double quantity,
+    required String category,
+    required List<File> images,
+  }) async {
+    try {
+      final cloudinary = CloudinaryPublic("ibuild", "f4qi5enp");
+      List<String> imageUrls = [];
+
+      for (int i = 0; i < images.length; i++) {
+        CloudinaryResponse res = await cloudinary
+            .uploadFile(CloudinaryFile.fromFile(images[i].path, folder: name));
+
+        imageUrls.add(res.secureUrl);
+      }
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
+  }
+}
